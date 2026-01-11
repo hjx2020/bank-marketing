0cd85a1eb6ef2e8beef244ecb7879ce2791a3d29
Hanjian Xu <hanjian.xu@mail.mcgill.ca>
2026-01-11 17:38:02 -0500

    streamlit interface adjusted

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 
 # 1. Page Config
 st.set_page_config(
-    page_title="Bank Marketing Predictor",
+    page_title="Term Deposit Subscription Predictor",
     page_icon="🏦",
     layout="wide",
     initial_sidebar_state="expanded"
@@ -73,36 +73,55 @@ with col1:
          'self-employed', 'services', 'student', 'technician', 'unemployed', 'unknown'])
     marital = st.selectbox("Marital Status", ['divorced', 'married', 'single', 'unknown'])
     education = st.selectbox("Education", 
-        ['basic.4y', 'basic.6y', 'basic.9y', 'high.school', 'illiterate', 
+        ['basic.4y', 'basic.6y', 'basic.9y', 'high.school', 
          'professional.course', 'university.degree', 'unknown'])
     
     st.markdown("#### Financial Products")
-    default = st.selectbox("Credit in Default?", ['no', 'yes', 'unknown'])
+    default = st.selectbox("Credit in Default?", ['no', 'unknown'])
     housing = st.selectbox("Housing Loan?", ['no', 'yes', 'unknown'])
     loan = st.selectbox("Personal Loan?", ['no', 'yes', 'unknown'])
 
 with col2:
-    st.header("📞 Contact & Campaign Attributes")
+    st.header("📞 Campaign Contact Info")
     # latest contact attributes
     contact = st.selectbox("Contact Type", ['cellular', 'telephone'])
+    # Last Contact month
+    month_codes = ['jan', 'feb', 'mar', 'apr', 'may', 'jun',
+               'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+
+    month_full_map = {
+        'jan': 'January', 'feb': 'February', 'mar': 'March',
+        'apr': 'April', 'may': 'May', 'jun': 'June',
+        'jul': 'July', 'aug': 'August', 'sep': 'September',
+        'oct': 'October', 'nov': 'November', 'dec': 'December'
+    }
+
     month = st.selectbox("Last Contact Month", 
-        ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'])
-    day_of_week = st.selectbox("Last Contact Day", ['mon', 'tue', 'wed', 'thu', 'fri'])
+        month_codes, index=4, format_func=lambda x: month_full_map[x])
+
+    # Last Contact Day
+    wod_codes = ['mon', 'tue', 'wed', 'thu', 'fri']
+
+    wod_full_map = {
+        'mon': 'Monday', 'tue': 'Tuesday', 'wed': 'Wednesday',
+        'thu': 'Thursday', 'fri': 'Friday'
+    }
+
+    day_of_week = st.selectbox("Last Contact Day", 
+        wod_codes, index = 0, format_func = lambda x: wod_full_map[x])
     duration = st.number_input("Duration (sec)", min_value=0, value=200, help="Last contact duration in seconds")
     # previous campaign attributes
-    campaign = st.number_input("Number of Contacts", min_value=1, value=1, help="Number of contacts during this campaign")
-    pdays = st.number_input("Days since last contact", min_value=0, value=999, help="999 means never contacted")
-    previous = st.number_input("Previous Contacts", min_value=0, value=0, help="Number of contacts before this campaign")
-    poutcome = st.selectbox("Previous Outcome", ['failure', 'nonexistent', 'success'])
+    campaign = st.number_input("Total Number of Contacts (Current Campaign)", min_value=1, value=1, help="Number of contacts during this campaign (minimum 1)")
+    pdays = st.number_input("Days Since Last Contact (Last Campaign)", min_value=0, value=999, help="999 means never contacted")
+    previous = st.number_input("Total Number of Contacts (Previous Campaigns)", min_value=0, value=0, help="Number of contacts before this campaign")
+    poutcome = st.selectbox("Deposit Outcome of the Last Campaign", ['failure', 'nonexistent', 'success'], index = 1)
 
 with col3:
-    st.header("📊 Socio-Economic Indicators")
+    st.header("📊 Socio-Economic Indices")
     # Socio-Economic Indicators
-    emp_var_rate = st.number_input("Emp. Var. Rate", value=-1.8)
-    cons_price_idx = st.number_input("Cons. Price Index", value=92.89)
-    cons_conf_idx = st.number_input("Cons. Conf. Index", value=-46.2)
-    euribor3m = st.number_input("Euribor 3 Month", value=1.25)
-    nr_employed = st.number_input("Nr. Employed", value=5099.1)
+    cons_price_idx = st.number_input("Consumer Price Index", value=92.89)
+    cons_conf_idx = st.number_input("Consumer Confidence Index", value=-46.2)
+    euribor3m = st.number_input("Euribor (3 Month Maturity)", value=1.25)
 
 
 # Prepare Data
@@ -112,8 +131,8 @@ data = {
     'contact': contact, 'month': month, 'day_of_week': day_of_week,
     'duration': duration, 'campaign': campaign, 'pdays': pdays,
     'previous': previous, 'poutcome': poutcome,
-    'emp.var.rate': emp_var_rate, 'cons.price.idx': cons_price_idx,
-    'cons.conf.idx': cons_conf_idx, 'euribor3m': euribor3m, 'nr.employed': nr_employed
+    'cons_price_idx': cons_price_idx,
+    'cons_conf_idx': cons_conf_idx, 'euribor3m': euribor3m
 }
 df = pd.DataFrame([data])
 
@@ -127,9 +146,11 @@ if b2.button("🚀 Predict Subscription Probability"):
     
     with st.spinner("Calculating..."):
         try:
-            prediction = model.predict(df)[0]
+            prediction0 = model.predict(df)[0]
             probability = model.predict_proba(df)[0][1]
-            
+            threshold = 0.644665
+            prediction = 1 if probability >= threshold else 0
+
             # Display Results (Centered below button)
             r1, r2, r3 = st.columns([1, 2, 1])
             
@@ -142,6 +163,8 @@ if b2.button("🚀 Predict Subscription Probability"):
                     st.error("## ❌ Low Likelihood of Subscription")
                     
                 st.metric(label="Subscription Probability", value=f"{probability:.2%}", delta=None)
+                st.caption(f"Decision threshold: {threshold:.2f}")
+                
                 st.progress(probability)
                 st.markdown("</div>", unsafe_allow_html=True)
 
